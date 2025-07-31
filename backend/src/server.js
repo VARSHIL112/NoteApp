fc5cece6c33a68a96d393564eba958c0ae47c247
VARSHIL112 <vjaskiya@gmail.com>
2025-07-31 14:04:53 +0530

    ready for deploymeant

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,47 @@
-import express from  "express";
-import router from "./routes/noteroutes.js";
-import { connectdb } from "./config/db.js";
-import dotenv from "dotenv";
-import { apiLimiter } from "./middleware/ratelimiter.js";
+import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
+import router from "./routes/noteroutes.js";
+import  {connectdb}  from "./config/db.js";
+import {apiLimiter} from "./middleware/ratelimiter.js";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors(
-    {
-        origin:"http://localhost:5173"
-    }
-));
-app.use(express.json());
-app.use(apiLimiter)
-app.use("/api/notes",router);
-connectdb().then(()=>{app.listen(PORT,()=>{console.log("server started on",PORT)});})
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(apiLimiter);
 
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
+app.use("/api/notes", router);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+connectdb().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
