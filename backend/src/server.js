f371b855d169d1d297ad84662ade93c358b5a611
VARSHIL112 <vjaskiya@gmail.com>
2025-08-01 13:30:45 +0530

    PATHE UPDATED

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,10 +33,10 @@ app.use(apiLimiter);
 app.use("/api/notes", router);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
 
